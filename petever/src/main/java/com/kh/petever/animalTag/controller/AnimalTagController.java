95f7314afeb203482364e0756d842a54a5adf5d5
rato12 <rato12@nate.com>
2020-10-08 01:11:21 +0900

    animalTag insert 성공

--- petever/src/main/java/com/kh/petever/animalTag/controller/AnimalTagController.java
+++ petever/src/main/java/com/kh/petever/animalTag/controller/AnimalTagController.java
@@ -3,12 +3,17 @@ package com.kh.petever.animalTag.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.kh.petever.animalTag.model.service.AnimalTagService;
+import com.kh.petever.animalTag.model.vo.AnimalTag;
+
+import lombok.extern.slf4j.Slf4j;
 
 @Controller
 @RequestMapping("/animalTag")
+@Slf4j
 public class AnimalTagController {
 	
 	@Autowired
@@ -18,6 +23,16 @@ public class AnimalTagController {
 	public String animalTagForm() {
 		
 		return "animalTag/animalTag";
+	}
+	
+	@PostMapping("/animalTagBuy.do")
+	public String animalTagBuy(AnimalTag aniTag) {
+		System.out.println(aniTag);
+
+		int result = animalService.insertAniTag(aniTag);
+		
+		
+		return "";
 	}
 	
 	
--- petever/src/main/java/com/kh/petever/animalTag/model/dao/AnimalTagDAOImpl.java
+++ petever/src/main/java/com/kh/petever/animalTag/model/dao/AnimalTagDAOImpl.java
@@ -4,10 +4,17 @@ import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import com.kh.petever.animalTag.model.vo.AnimalTag;
+
 @Repository
 public class AnimalTagDAOImpl implements AnimalTagDAO {
 	
 	@Autowired
 	private SqlSession sqlSession;
+
+	@Override
+	public int insertAniTag(AnimalTag aniTag) {
+		return sqlSession.insert("animalTag.insertAniTag", aniTag);
+	}
 
 }
