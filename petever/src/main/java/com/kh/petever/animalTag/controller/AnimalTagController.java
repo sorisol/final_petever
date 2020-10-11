f89ba324d7b4324e91a0253323ae83e98c5174c9
rato12 <rato12@nate.com>
2020-10-12 02:25:01 +0900

    kakaoPay 연동2

--- petever/src/main/java/com/kh/petever/animalTag/controller/AnimalTagController.java
+++ petever/src/main/java/com/kh/petever/animalTag/controller/AnimalTagController.java
@@ -2,9 +2,11 @@ package com.kh.petever.animalTag.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.kh.petever.animalTag.model.service.AnimalTagService;
 import com.kh.petever.animalTag.model.vo.AnimalTag;
@@ -15,24 +17,40 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/animalTag")
 @Slf4j
 public class AnimalTagController {
-	
+
 	@Autowired
 	private AnimalTagService animalService;
 
 	@GetMapping("/animalTag.do")
 	public String animalTagForm() {
-		
+
 		return "animalTag/animalTag";
 	}
-	
-	@PostMapping("/animalTagBuy.do")
-	public String animalTagBuy(AnimalTag aniTag) {
-		System.out.println(aniTag);
 
+	@PostMapping("/insertAniTag.do") 
+	public String animalTagBuy(AnimalTag aniTag, @RequestParam int tagPrice) { 
+		log.debug("insert 진입");
+		System.out.println(aniTag);
+		log.debug("tagPrice{}", tagPrice);
+	 
 		int result = animalService.insertAniTag(aniTag);
 		
-		return "";
+		log.debug("등록여부 {}", result == 1 ? "성공" : "실패");
+	 
+		return "/"; 
 	}
-	
-	
+	 
+
+	@PostMapping("/animalTagPay.do")
+	public String animalTagPay(AnimalTag aniTag, @RequestParam int tagPrice, Model model) {
+		log.debug("cacaoPay 진입");
+		System.out.println(aniTag);
+		log.debug("tagPrice{}", tagPrice);
+		
+		model.addAttribute("aniTag", aniTag);
+		model.addAttribute("tagPrice", tagPrice);
+
+		return "animalTag/animalTagPay";
+	}
+
 }
